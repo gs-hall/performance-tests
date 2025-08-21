2f4e1366fc476cc5835af4f4bc18598ad94c406b
gs-hall <egorivantsov@gmail.com>
2025-08-21 23:57:37 +0300

    grpc new_user_make_top_up_operation

--- upload_results.sh
+++ upload_results.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scenario=get_documents
-#locust --config=./scenarios/grpc/gateway/new_user_$scenario/v1.0.conf
+scenario=make_top_up_operation
+locust --config=./scenarios/grpc/gateway/new_user_$scenario/v1.0.conf
 locust --config=./scenarios/grpc/gateway/new_user_$scenario/v1.0.conf --show-task-ratio-json > locust_grpc_gateway_new_user_${scenario}_ratio.json
 load-testing-hub upload-locust-report --yaml-config=./scenarios/grpc/gateway/new_user_$scenario/load_testing_hub.yaml
